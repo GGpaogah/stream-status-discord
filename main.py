0ed020a1c9772e3a148304196128f3eabc087c72
paogah <70354037+haruto342@users.noreply.github.com>
2022-01-30 13:57:29 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
-import discord, os , keep_alive , asyncio , datetime ,pytz
+import discord, os , alive , asyncio , datetime ,pytz
 
 
 from discord.ext import tasks, commands
 
 client = commands.Bot(
-  command_prefix='zz!',
+  command_prefix='!',
   self_bot=True
 )
 
 
 
-
+# name = your status and url = your your twitch link
 @client.event
 async def on_connect():
   await client.change_presence(activity = discord.Streaming(name = 
-  "abit sangat sadboy pemirsa", url = "https://twitch.tv/paogah"))
+  "status", url = "https://twitch.tv/paogah"))
 
 
 
-keep_alive.keep_alive()
+alive.alive()
 client.run(os.getenv("TOKEN"), bot=False)
